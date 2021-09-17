33bb07850c48af9852eacd1cbd01e0c4eff01e5b
Konrad <35558461+Konrad1991@users.noreply.github.com>
2021-09-17 08:52:30 +0200

    update how r can be passed to fortran

--- src/callRfromFortran.cpp
+++ src/callRfromFortran.cpp
@@ -12,17 +12,25 @@ void Rfct(SEXP fct) {
   f();
 }
 
+
+// It does not work why? --> has to be in function
+//Rcpp::Environment global = Rcpp::Environment::global_env();;
+//Rcpp::Function hello2 = global.get("hello");
+
+
 // R function has to be part f package
 Rcpp::Environment pkg = Rcpp::Environment::namespace_env("fortranfctpassing");
-Rcpp::Function f = pkg["hello"];
+Rcpp::Function f = pkg.get("hello");
 
 // [[Rcpp::export]]
 void Rfct2() {
   f();
+
+  // Has to be in function
+  Rcpp::Environment global = Rcpp::Environment::global_env();;
+  Rcpp::Function hello2 = global.get("hello");
+  hello2();
 }
-
-
-
 
 // this is called by fortran later
 extern "C" {
